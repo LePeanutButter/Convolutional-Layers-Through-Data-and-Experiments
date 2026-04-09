34a735a080a4c78aa52ebc51ca02850a9dbc484a
Santiago Botero <157855016+LePeanutButter@users.noreply.github.com>
2026-04-08 23:11:42 -0500

    feat(lambda-java): add full function app, final JAR, and deployment README
    - Added complete Java Lambda function application
    - Included the compiled `.jar` file for deployment
    - Added README documenting AWS Lambda & API Gateway deployment steps

---
+++ src/main/java/co/edu/escuelaing/services/User.java
@@ -0,0 +1,11 @@
+package co.edu.escuelaing.services;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class User {
+    private String name;
+    private String email;
+}
